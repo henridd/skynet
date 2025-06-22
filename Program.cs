9bbfd101d32b30a2471af07b2d6e0176e646c66f
henridd <henri.oo@hotmail.com>
2025-06-22 10:20:46 +0200

    First test

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+var randomCounter = 0;
+for (int i = 0; i < 10; i++)
+    randomCounter = i;
+
+Console.WriteLine($"Hello, World! {10}");
